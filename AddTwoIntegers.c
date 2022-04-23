e97c74f34de8d6afe634e96f4efaa5a1375cf2c0
Berke Altıparmak <44147280+balpa@users.noreply.github.com>
2022-04-23 10:45:02 +0300

    Create AddTwoIntegers.c

---
+++ AddTwoIntegers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2;
+    printf("Enter the first number: ");
+    scanf("%d", &number1);
+    printf("Enter the second number: ");
+    scanf("%d", &number2);
+    printf("Sum equals: %d\n", number1 + number2);
+    return 0;
+}
