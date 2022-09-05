e9cee5d130936bea55c5bfea1de650f5914f9782
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-09-05 16:55:55 +0300

    working on func time delta in C

--- CWorkspace.c
+++ CWorkspace.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <time.h>
+
+time_t get_time();
 
 int main()
 {
@@ -13,11 +16,36 @@ int main()
     float money_amount;
   };
 
-  struct Account {
+  struct Account
+  {
     int accountID;
     double total_amount;
     int max_people;
   };
 
+  /*printf("Time in Milliseconds: %ld\n", get_time());*/
+
+  clock_t t0;
+  clock_t t1;
+  clock_t delta_time;
+  double res;
+
+  t0 = clock();
+
+  for (int i = 0; i < 10000000; i++)
+  {
+    res = res + i;
+  }
+  t0 = clock() - t0;
+
+  printf("Delta time: %ld milliseconds\n", t0 / 1000);
+
   return 0;
+};
+
+time_t get_time()
+{
+  time_t seconds;
+  seconds = time(NULL);
+  return seconds;
 };
