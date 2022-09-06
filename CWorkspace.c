80ccfc929bdc727218537627b7b2457fd514dcf3
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-09-06 16:51:44 +0300

    indentation and elapsed time

--- CWorkspace.c
+++ CWorkspace.c
@@ -12,32 +12,27 @@ int main(){
   double res;
 
   t0 = clock();
-  for (int i = 0; i < 10000000; i++){
-    res = res + i;
-  }
+  create_random_array(-10, 80, 10000);  
   t0 = clock() - t0;
   printf("Delta time: %ld milliseconds\n", t0 / 1000);
-
-  create_random_array(-10, 80, 100);
 
   return 0;
 };
 
 void create_random_array(int first_val, int second_val, int range){
 
-    int rand_array[range];
-    for (int i = 0; i < range; i++){
-      rand_array[i] = (rand() % (first_val - second_val + 1)) + first_val;
-    };
-    for (int x = 0; x < range; x++){
-      if(x==range-1){
-        printf("%d\n", rand_array[x]);
-      }
-      else {
-        printf("%d, ",rand_array[x]);
-      }
-    };
-
+  int rand_array[range];
+  for (int i = 0; i < range; i++){
+    rand_array[i] = (rand() % (first_val - second_val + 1)) + first_val;
+  };
+  for (int x = 0; x < range; x++){
+    if(x==range-1){
+      printf("%d\n", rand_array[x]);
+    }
+    else {
+      printf("%d, ",rand_array[x]);
+    }
+  };
 };
 
 time_t get_time(){
